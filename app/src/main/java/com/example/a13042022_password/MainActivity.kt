619bbac47dca29c9ba3dcc61f6b03cb2d658162f
piasek <piaskowski3456@gmail.com>
2022-04-13 07:55:28 +0200

    Zapisywanie hasla skonczone

--- app/src/main/java/com/example/a13042022_password/MainActivity.kt
+++ app/src/main/java/com/example/a13042022_password/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.a13042022_password
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import android.widget.Toast.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn_zat = findViewById<Button>(R.id.btnZatwierdz) as Button
+        var btn_spr = findViewById<Button>(R.id.btnSpr) as Button
+        var haslo1 = findViewById<EditText>(R.id.haslo1) as EditText
+        var haslo2 = findViewById<EditText>(R.id.haslo2) as EditText
+
+
+        btn_zat.setOnClickListener {
+            val haslo = haslo1.text;
+            val text = Toast.makeText(applicationContext , "Haslo zostalo zaspisane", Toast.LENGTH_SHORT)
+            text.show()
+        }
     }
 }
